eba075c90cf8d56da4ceed776c7991eb6ebc64ec
David Podhola <david@podhola.net>
2018-05-16 19:59:59 +0200

    Update TestGetSimpleOrder.kt

--- test/kotlin/TestGetSimpleOrder.kt
+++ test/kotlin/TestGetSimpleOrder.kt
@@ -6,14 +6,14 @@ import software.hsharp.woocommerce.impl.*
 class TestGetSimpleOrder {
     @Test
     fun getSimpleOrder() {
-        val config = Config("https://vinokutnahora.cz/", "ck_0e580f4ff4b5fc394a8b7774ac0a02b67f6ea8f5", "cs_167a4e956e98ddc577e372812998c02db9b383c3")
+        val config = Config("https://testsite.cz/", "ck_XXX", "cs_YYY")
         val wooCommerce = WooCommerceAPI(config, ApiVersionType.V2)
         val order : IOrder = wooCommerce.getOrder(1290)
         println("Order:$order")
     }
     @Test
     fun getAllOrders() {
-        val config = Config("https://vinokutnahora.cz/", "ck_0e580f4ff4b5fc394a8b7774ac0a02b67f6ea8f5", "cs_167a4e956e98ddc577e372812998c02db9b383c3")
+        val config = Config("https://testsite.cz/", "ck_XXX", "cs_YYY")
         val wooCommerce = WooCommerceAPI(config, ApiVersionType.V2)
         val orders : Array<IOrder> = wooCommerce.getOrders()
         orders.forEach { println("Order:$it") }
